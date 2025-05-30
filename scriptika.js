36440483a9983b497db30e6ee7d05c1dd3512560
ikana82 <ikanurasih82@gmail.com>
2025-05-30 12:43:23 +0700

    Menambahkan function del

--- scriptika.js
+++ scriptika.js
@@ -1,14 +1,15 @@
 function appendValue(val) {
-    let displayValue = document.getElementById("display").value 
+    let displayValue = document.getElementById("display").value // string
 
     if (displayValue === "Error!") {
-        document.getElementById("display") = ""
-    } 
-    if (displayValue === "0") {
-        document.getElementById("display").value = val 
-    } else {
-        document.getElementById("display").value += val;
+        document.getElementById("display").value =""
     }
+    if (displayValue === "0") {
+        document.getElementById("display").value = val // reassignment
+    } else {
+        document.getElementById("display").value += val; // concatination
+    }
+    
 }
 
 function clearDisplay() {
@@ -19,8 +20,27 @@ function calculate() {
     let displayValue = document.getElementById("display").value
     try {
         let result = eval(displayValue)
-        document.getElementById("display").value = result 
+        document.getElementById("display").value = result
     } catch (error) {
         document.getElementById("display").value = "Error!"
+    }
+}
+
+// Sama saja dengan onclick="appendValue"
+const addButton = document.getElementById("add-button")
+addButton.addEventListener("click", function () {
+    appendValue("+")
+})
+
+function del() {
+    let display = document.getElementById("display");
+    let displayValue = display.value;
+
+    // Jika angka habis atau kosong, maka ganti ke 0
+    if (displayValue === "Error!" || displayValue.length === 1) {
+        display.value = "0";
+    } else {
+        // Hapus satu karakter paling akhir
+        display.value = displayValue.slice(0, -1);
     }
 }
